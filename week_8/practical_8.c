259d00480d5b4e11ed1e96a4c3f5fb9904901e15
Mark Quigley <markquigley@OnePlus3T.station>
2021-11-19 10:44:21 +0000

    Finished week_8/
    Finished comments and ran on server

--- week_8/practical_8.c
+++ week_8/practical_8.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int GCD_itterative (int a, int b){
-    int temp;
-    while (b != 0){
+    int temp;           //holds value of b when updateing
+    while (b != 0){     //only if GCD has not been reached
         temp = b;
         b = a%b;
         a = temp;
@@ -10,31 +10,40 @@ int GCD_itterative (int a, int b){
     return a;
 }
 
-int GCD_recursive (int a, int b){
-    //printf ("\ta = %d\tb = %d\n", a, b);
-    int GCD, temp;
-    if (b != 0){
-        temp = b;
-        b = a%b;
-        a = temp;
-        GCD = GCD_recursive (a, b);
+int GCD_recursive (int* a, int* b){
+/*Pass by referenct to avoid creation of new varaibles each time
+  Only new variable created is temp*/
+    int temp;
+    if (*b != 0){   //when GCD was not been reached
+        temp = *b;
+        (*b) = (*a)%(*b);
+        (*a) = temp;
+        return GCD_recursive (a, b);    //quick escape when returned value passed
     }
-    else {
-        return a;
+    else {          //when GCD has been reached
+        return *a;
     }
-    return GCD;
 }
 
 
 int main (int argc, char* argv[]){
+    //input values
     int a = 222;
     int b = 102;
-    //222,102 = 6
+    //222,102 = 6       //test values
     //8, 12 = 4
     
+    //find GCD by both functions
     int GCD_itt = GCD_itterative (a, b);
-    int GCD_rec = GCD_recursive (a, b);
+    int a_pass = a, b_pass = b;     //pass these so a & b remain unchanged
+    int GCD_rec = GCD_recursive (&a_pass, &b_pass);
     
+    //print output
     printf ("GCD_itt = %d\n", GCD_itt);
     printf ("GCD_rec = %d\n", GCD_rec);
+    
+    return 0;
 }
+
+//gcc practical_8.c -o p_8.out -Wall
+//./p_8.out 
