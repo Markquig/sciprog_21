59b0bd7d64b54dc864e262f5b7c913c4ed122c71
Mark Quigley <markquigley@OnePlus3T.station>
2021-11-19 10:36:16 +0000

    Updated week_7/
    Finished coments and ran on server. Still to answer last part of question about acuricy reducing.

--- week_7/practical_7.c
+++ week_7/practical_7.c
@@ -8,12 +8,13 @@ int* allocatearray (int size) {
 }
 
 void deallocatearray (int* array) {
+/*frees memory allocated in allocatearray*/
     free (array);
     return;
 }
 
 void fillwithones (int* array, int size){
-/*sets all elements of 1d array to 1's*/
+/*loops through 1d array and sets all elements to 1*/
     int i;
     for (i = 0; i < size; i++){
         array[i] = 1;
@@ -23,7 +24,7 @@ void fillwithones (int* array, int size){
 }
 
 void printarray (int* array, int size){
-/*prints 1d array*/
+/*loops through 1d array to print each element*/
     int i;
     for (i = 0; i < size; i++){
         printf ("%d, ", array[i]);
@@ -45,6 +46,8 @@ double* approx_e (int* x,int size){
 }
 
 void deallocate_approx_e (double* array){
+/*could have used deallocatearray above, but i like to have a seporate function
+   incase approx_e changes*/
     free(array);
 }
 
@@ -59,13 +62,16 @@ void printarray_double (double* array, int size){
 }
 
 void print_results (double* array, int size){
-/*prints 1d array*/
+/*prints results of approximation & error*/
     int i;
     for (i = 0; i < size; i++){
         printf ("\tf(%d) = %lf\t\tf(%d) - e = %.5e\n",
-                i, array[i], i, array[i]-exp(1));
+                i,                  //order of approximation
+                array[i],           //approximation
+                i,                  //order of approximation
+                array[i]-exp(1));   //error in approximation
     }
-    printf ("\n");
+    printf ("\n");      //end output
     return;
 }
 
@@ -78,18 +84,19 @@ int main (int argc, char* argv[]){
     scanf("%d", &size);
     
     int* A;
-    A = allocatearray (size);
-    //*
-    //printarray (A, size);
-    fillwithones (A, size);
-    //printarray (A, size);//*/
+    A = allocatearray (size);   //create memory to hold values
+    fillwithones (A, size);     //initalise elements to 1's
     
-    double* f = approx_e(A, size);
-    //printarray_double (f, size+1);
-    print_results (f, size+1);
+    double* f = approx_e(A, size);  //returns array with approximates of e
+    printarray_double (f, size+1);  //print array f
+    print_results (f, size+1);      //print f with index & error in approx
     
+    //free memory
     deallocate_approx_e (f);
     deallocatearray (A);
     
     return 0;
 }
+
+//gcc practical_7.c -o p_7.out -lm -Wall
+//./p_7.out 
